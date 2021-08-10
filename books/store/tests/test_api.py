2be70b6efa2a0ed39fe36a9bb1a574e04a749dbd
gretchenau <gretchenaussie@gmail.com>
2021-08-10 23:17:59 +0300

    filter

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -6,12 +6,22 @@ from store.serializers import BookSerializer
 
 
 class BooksApiTestCase(APITestCase):
+    def setUp(self):
+        self.book_1 = Book.objects.create(name='Book1', price=25)
+        self.book_2 = Book.objects.create(name='Book2', price=25)
+
     def test_get(self):
-        book_1 = Book.objects.create(name='Book1', price=25)
-        book_2 = Book.objects.create(name='Book2', price=25)
         url = reverse('book-list')
         print(url)
         response = self.client.get(url)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(BookSerializer([book_1, book_2], many=True).data, response.data)
+        self.assertEqual(BookSerializer([self.book_1, self.book_2], many=True).data, response.data)
+        print(response.data)
+
+    def test_get2(self):
+        url = reverse('book-list')
+        print(url)
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(BookSerializer([self.book_1, self.book_2], many=True).data, response.data)
         print(response.data)
